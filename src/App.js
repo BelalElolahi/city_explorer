0e9dd7d6cb6631edf58bcb9c5000b3ee9f45b163
BelalElolahi <belalelolahi003@gmail.com>
2021-09-12 20:31:39 -0700

    finish lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SerchForm from './component/SerchForm';
+import Location from './component/Location';
+import axios from 'axios';
+
+export class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameOfCity: "",
+            lat: "",
+            lon: "",
+            zoom:0,
+            showData: false,
+            error:""
+            
+            
+        };
+    }
+
+
+    handelInputLocation = (e) => {
+
+        let nameOfCity = e.target.value;
+        this.setState({
+            nameOfCity: nameOfCity
+        });
+        console.log(this.state.nameOfCity);
+    }
+
+    handelSubmit = (e) => {
+        e.preventDefault();
+
+        let config = {
+            method: "GET",
+            baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.nameOfCity}`
+        };
+
+        
+    
+
+        console.log(config);
+
+        axios(config).then(res => {
+            let response = res.data[0];
+            console.log(response);
+
+           
+                this.setState({
+                    nameOfCity: response.display_name,
+                    lat: response.lat,
+                    lon: response.lon,
+                    zoom:18,
+                    showData: true
+                    
+                });
+
+            
+           
+        });
+    }
+
+
+
+    render() {
+        return (
+            <div >
+                <SerchForm handelInputLocation={this.handelInputLocation} handelSubmit={this.handelSubmit} />
+
+                {
+                    this.state.showData&&
+                    
+                    <Location  cityName ={this.state.nameOfCity} lat={this.state.lat} lon={this.state.lon} imgeUrl={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&size=100px250&zoom=${this.state.zoom}&markers=${this.state.lat},${this.state.lon}|icon:large-blue-cutout&format=png`}/>
+                    
+                        
+                     
+                }
+                     
+                     
+                     
+
+
+            </div>
+        );
+    }
 }
 
 export default App;
