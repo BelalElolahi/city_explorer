526bb5cf6d5969276289c1c6a55f1ffd3de4a4cd
BelalElolahi <belalelolahi003@gmail.com>
2021-09-15 17:00:35 -0700

    refactoring code

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
 
 export class Movie extends Component {
     render() {
         return (
             <div className="row">
-
-
+                  
+                  
                    {
                         this.props.movieData.map(item => {
                             console.log(item);
                             
                             <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="/100px180" />
+                            <Card.Img variant="top" src={item.imageUrl}/>
                             <Card.Body>
                                 <Card.Title>{item.title}</Card.Title>
-                                <Card.Text>
-                                    
+                                <Card.Text> 
+                                overview : {item.overview} /
+                                averageVotes : {item.averageVotes} /
+                                totalVotes : {item.totalVotes} / 
+                                popularity : {item.popularity} / 
+                                releasedOn : {item.releasedOn}
                                 </Card.Text>
                                
                             </Card.Body>
